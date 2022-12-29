1860eaacc3a961ca3e01e0b15b36980572cbf704
serhiibabanov <serhii.babanov@gmail.com>
2022-12-28 20:06:27 -0500

    refactoring NoteController + templates. Fix missing path on Unix OS

--- src/main/java/ua/goit/dev6/note/NoteController.java
+++ src/main/java/ua/goit/dev6/note/NoteController.java
@@ -1,62 +1,61 @@
 package ua.goit.dev6.note;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
-import ua.goit.dev6.account.UserDAO;
-import ua.goit.dev6.account.UserDTO;
 import ua.goit.dev6.account.UserService;
-import ua.goit.dev6.config.UserPrincipal;
 
 import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 @RequestMapping("/notes")
 public class NoteController {
     private final NoteService noteService;
     private final UserService userService;
 
 
-    @GetMapping("/findAll")
+    @GetMapping
     private ModelAndView findAll(){
-        ModelAndView result = new ModelAndView("/notes/notes");
+        ModelAndView result = new ModelAndView("notes/notes");
         List<NoteDTO> notes = noteService.findAll();
         result.addObject("notes", notes);
         return result;
     }
 
     @GetMapping("/create")
-    private ModelAndView getCreateForm() {
-        ModelAndView result = new ModelAndView("/notes/createNoteForm");
-        NoteDTO noteDTO = new NoteDTO();
-        result.addObject("note", noteDTO);
+    private String getCreateForm() {
+        return "notes/createNoteForm";
+    }
+
+    @GetMapping("/{id}")
+    private ModelAndView getEditForm(@PathVariable("id") UUID id) {
+        ModelAndView result = new ModelAndView("notes/editNoteForm");
+        NoteDTO note = noteService.findById(id);
+        result.addObject("note", note);
         return result;
     }
 
-    @GetMapping("/edit")
-    private ModelAndView getEditForm(@RequestParam("id") String id) {
-        ModelAndView result = new ModelAndView("/notes/editNoteForm");
-        NoteDTO editNote = noteService.findById(UUID.fromString(id));
-        result.addObject("note", editNote);
-        return result;
-    }
-
-    @PostMapping("/save")
-    private RedirectView save(@Validated @ModelAttribute("note") NoteDTO note){
+    @PostMapping
+    private String save(@Validated @ModelAttribute("note") NoteDTO note){
         note.setUser(userService.getAuthorizedUser());
         noteService.save(note);
-        return new RedirectView("/notes/findAll");
+        return "redirect:/notes";
     }
 
-    @GetMapping("/delete")
-    private RedirectView delete(@RequestParam("id") String id) {
-        noteService.deleteById(UUID.fromString(id));
-        return new RedirectView("/notes/findAll");
+    @DeleteMapping("/{id}")
+    private void delete(@PathVariable("id") UUID id) {
+        noteService.deleteById(id);
+        throw new ResponseStatusException(HttpStatus.OK, "Note deleted");
+    }
+    @ModelAttribute("note")
+    private NoteDTO getDefaultProduct() {
+        return new NoteDTO();
     }
 
 }
