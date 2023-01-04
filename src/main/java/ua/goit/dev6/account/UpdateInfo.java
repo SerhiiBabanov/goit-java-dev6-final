66c26d443fb4fe2e4a38df465d74b7905502acbc
serhiibabanov <serhii.babanov@gmail.com>
2023-01-03 19:03:07 -0500

    move business logic from controller to services

--- src/main/java/ua/goit/dev6/account/UpdateInfo.java
+++ src/main/java/ua/goit/dev6/account/UpdateInfo.java
@@ -1,64 +1,42 @@
 package ua.goit.dev6.account;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ua.goit.dev6.config.UserPrincipal;
-import ua.goit.dev6.roles.RoleService;
-import ua.goit.dev6.signin.SecurityService;
-import ua.goit.dev6.signup.UserValidator;
 
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class UpdateInfo {
-    private final PasswordEncoder passwordEncoder;
     private final UserService userService;
-    private final RoleService roleService;
-    private final SecurityService securityService;
-    private final UserValidator userValidator;
+
 
     @GetMapping("/settings/change")
-    public String changePassword(Model model) {
+    public String changePassword() {
         return "account/change";
     }
 
     @PostMapping("/settings/change")
-    public String changePassword(Model model, Authentication authentication,
-                                 @RequestParam(value = "oldPassword") String oldPassword,
+    public String changePassword(@RequestParam(value = "oldPassword") String oldPassword,
                                  @RequestParam(value = "newPassword") String newPassword,
-                                 @RequestParam(value = "repeatPassword") String repeatPassword) {
-        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
-        String userEmail = principal.getUsername();
+                                 @RequestParam(value = "repeatPassword") String repeatPassword,
+                                 BindingResult bindingResult) {
 
-        List<UserDTO> byName = userService.findByName(userEmail);
-        UserDTO user = byName.get(0);
-
-        model.addAttribute("user", user);
-
-        if (!passwordEncoder.matches(oldPassword, user.getPassword())) {
-            //!passwordEncoder.matches(passwordEncoder.encode(oldPassword), user.getPassword())) {
-            model.addAttribute("oldPasswordErr", "Old password is invalid!");
-            return "account/change";
-        } else if (!newPassword.equals(repeatPassword)) {
-            model.addAttribute("repeatPasswordErr", "New passwords aren't equals!");
-            return "account/change";
-        } else if (newPassword.length() < 8 || newPassword.length() > 100) {
-            model.addAttribute("passwordErr", "Please use between 8 and 100 characters!");
+        if (!userService.checkPassword(oldPassword)){
+            bindingResult.rejectValue("oldPassword", "Wrong password");
             return "account/change";
         }
 
-        user.setPassword(newPassword);
-        userService.save(user);
+        userService.changePassword(bindingResult, newPassword, repeatPassword);
+
+        if (bindingResult.hasErrors()) {
+            return "account/change";
+        }
 
         return "redirect:/";
+
     }
 }
