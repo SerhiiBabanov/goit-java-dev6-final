dadc1f07fd3060bb20e7bd3f996113b256d61d3d
svetl <svetlana.akulowa@gmail.com>
2023-01-08 20:58:12 +0200

    secured for friends

--- src/main/java/ua/goit/dev6/friend/FriendController.java
+++ src/main/java/ua/goit/dev6/friend/FriendController.java
@@ -1,6 +1,7 @@
 package ua.goit.dev6.friend;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ua.goit.dev6.account.UserDTO;
 import ua.goit.dev6.account.UserService;
-import ua.goit.dev6.note.AccessType;
 import ua.goit.dev6.note.NoteDTO;
 import ua.goit.dev6.note.NoteService;
 
@@ -16,13 +16,14 @@ import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
+@Secured(value = {"ROLE_ADMIN"})
 @Controller
 @RequestMapping("/friends")
 public class FriendController {
     private final UserService userService;
     private final NoteService noteService;
     @GetMapping
-    private ModelAndView findAllFriend(){
+    public ModelAndView findAllFriend(){
         ModelAndView result = new ModelAndView("friends/friends");
         UserDTO authorizedUser = userService.getAuthorizedUser();
         result.addObject("users",userService.allFriend(authorizedUser));
@@ -30,7 +31,7 @@ public class FriendController {
     }
 
     @GetMapping("/add")
-    private ModelAndView potentialFriends() {
+    public ModelAndView potentialFriends() {
         ModelAndView result = new ModelAndView("friends/addFriend");
         result.addObject("users", userService.listAll());
         return result;
@@ -42,12 +43,12 @@ public class FriendController {
     }
 
     @GetMapping("/delete/{id}")
-    private String delete(@PathVariable("id") UUID id) {
+    public String delete(@PathVariable("id") UUID id) {
         userService.deleteFriend(userService.getAuthorizedUser().getId(),id);
         return "redirect:/friends";
     }
     @GetMapping("/view/{id}")
-    private ModelAndView viewNoteFriend(@PathVariable("id") UUID id) {
+    public ModelAndView viewNoteFriend(@PathVariable("id") UUID id) {
         ModelAndView result = new ModelAndView("friends/friendNotes");
         List<NoteDTO> notes = noteService.findPublicByUserId(id);
         result.addObject("notes", notes);
